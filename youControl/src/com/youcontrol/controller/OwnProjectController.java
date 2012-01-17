2b3a22a179eb9f6d73a8cd74a24e42d4c23a641b
mondor <mondor@gmail.com>
2012-01-17 22:53:48 +0000

    Refactory activityController

--- youControl/src/com/youcontrol/controller/OwnProjectController.java
+++ youControl/src/com/youcontrol/controller/OwnProjectController.java
@@ -1,7 +1,6 @@
 package com.youcontrol.controller;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import br.com.caelum.vraptor.Get;
@@ -11,13 +10,8 @@ import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.view.Results;
 
-import com.youcontrol.dao.ActivityDao;
-import com.youcontrol.dao.CommentActivityDao;
-import com.youcontrol.dao.ProjectDao;
 import com.youcontrol.dao.UserDao;
 import com.youcontrol.dao.UserProjectsDao;
-import com.youcontrol.model.Activity;
-import com.youcontrol.model.CommentActivity;
 import com.youcontrol.model.Project;
 import com.youcontrol.model.User;
 import com.youcontrol.model.UserProjects;
@@ -29,26 +23,17 @@ public class OwnProjectController {
 	
 	private final Result result;
 	private final UserWeb userWeb;
-	private final ProjectDao projectDao;
-	private final ActivityDao activityDao;
 	private final UserDao userDao;
 	private final UserProjectsDao userProjectsDao;
-	private final CommentActivityDao commentActivityDao;
 	
 	public OwnProjectController(Result result, 
 								UserWeb userWeb, 
-								ProjectDao projectDao, 
-								ActivityDao activityDao,
 								UserDao userDao,
-								UserProjectsDao userProjectsDao,
-								CommentActivityDao commentActivityDao) {
+								UserProjectsDao userProjectsDao) {
 		this.result = result;
 		this.userWeb = userWeb;
-		this.projectDao = projectDao;
-		this.activityDao = activityDao;
 		this.userDao = userDao;
 		this.userProjectsDao = userProjectsDao;
-		this.commentActivityDao = commentActivityDao;
 	}
 
 	@Get @Path("/projects/{project.id}")
@@ -83,51 +68,4 @@ public class OwnProjectController {
 		result.use(Results.json()).from(retorno).serialize();
 	}
 	
-	@Get @Path("/projects/{project.id}/activity")
-	public void activity(Project project) {
-		result.include("atividades", projectDao.listarAtividades(project));
-	}
-	@Get @Path("/activity/{activity.id}")
-	public void getActivity(Activity activity) {
-		result.include("atividade", activityDao.carregar(activity));
-		List<CommentActivity> comentarios = commentActivityDao.listar(activity);
-		result.include("comentarios", comentarios);
-		result.include("qtdComentarios", comentarios.size());
-	}
-	
-	@Get @Path("/projects/{project.id}/activity/new")
-	public void newActivity(Project project) {
-		Project projectVersion = this.projectDao.get(userWeb.getProject());
-		result.include("versions", projectVersion.getVersions());
-		
-		result.include("usuarios", userProjectsDao.listarUsuariosDoProj(project));
-	}
-	@Post @Path("/projects/{project.id}/activity/new")
-	public void createActivity(Activity activity, Project project, Long[] versions) {
-		if (activity.getResponsavel().getId() == null) activity.setResponsavel(null);
-		
-		Date date = new Date();
-		activity.setDataCriacao(date);
-		
-		activity.setCriador(userWeb.getUser());
-		activity.setProjeto(project);
-		
-		System.out.println("responsavel: " + activity.getResponsavel());
-		System.out.println("resumo: " + activity.getResumo());
-		
-		activityDao.criarAtividade(activity);
-		
-		result.redirectTo(this).activity(project);
-	}
-	
-	@Post @Path("/activity/{activity.id}/comment")
-	public void comment(CommentActivity comment, Activity activity) {
-		comment.setUsuario(userWeb.getUser());
-		comment.setAtividade(activity);
-		comment.setDataCriacao(new Date());
-		
-		commentActivityDao.salvar(comment);
-		
-		result.redirectTo(this).getActivity(activity);
-	}
 }
